e3050155b4d89e93da404dec5ad4932833150734
recchan13 <93rezao@gmail.com>
2020-07-10 03:16:31 +0700

    order dan kasir

--- kasir.php
+++ kasir.php
@@ -18,15 +18,56 @@
                 KASIR
             </div>
             <div class="row">
-                <div class="big-card">
-                    <h4>Aktivasi Barcode</h4>
-                    <!-- <button type="button" class="btn btn-add"><i class="fa fa-plus"></i></button>
-                    <P>tabel disini</P> -->
+                <div class="flex-card grow1">
+                    <h4>Aktivasi Barcode
+                        <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                        </label>
+                    </h4>
+                    <div class="grey-card">
+                        <i class="fa fa-qrcode"></i>
+                    </div>
+                    <select id="group" name="group">
+                        <option value="" disabled selected>Barang</option>
+                        <option value="barang1">barang1</option>
+                        <option value="barang2">barang2</option>
+                        <option value="barang3">barang3</option>
+                    </select>
+                    <p>tabel disini</p>
                 </div>
-                <div class="big-card">
+                <div class="flex-card grow2">
                     <h4>Ringkasan</h4>
-                    <!-- <button type="button" class="btn btn-add"><i class="fa fa-plus"></i></button>
-                    <P>tabel disini</P> -->
+                    <form action="kategori.php" id="kategori_tambah">
+                        <div class="form-card">
+                            <p>Pelanggan</p>
+                            <input type="text" id="email" name="email" placeholder="Email">
+                            <input type="text" id="phone" name="phone" placeholder="Nomor Telpon">
+                        </div>
+                        <div class="form-card">
+                            <p>Diskon</p>
+                            <input type="text" id="diskon" name="diskon" placeholder="0">
+                        </div>
+                        <div class="order-total">
+                            <h4>Total Keseluruhan</h4>
+                            <p>Rp. 0</p>
+                        </div>
+                        <div class="form-card">
+                            <p>Pembayaran</p>
+                            <input type="text" id="bayar" name="bayar" placeholder="Bayar">
+                            <input type="text" id="kembalian" name="kembalian" placeholder="Kembalian">
+                            <select id="metode" name="metode">
+                                <option value="" disabled selected>Metode Pembayaran</option>
+                                <option value="metode1">metode1</option>
+                                <option value="metode2">metode2</option>
+                                <option value="metode3">metode3</option>
+                            </select>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-submit" form="order_tambah" value="Submit">Order</button>
+                        <button type="button" class="btn btn-cancel" onclick="window.location.href='order.php'">Cancel</button>
+                    </form>
+                    
                 </div>
             </div>
         </div>
