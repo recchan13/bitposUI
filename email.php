92e62b3ee8d0a12c91a6e5d27b2f2c4b2cb79275
Malleya <44130156+Alleyaa@users.noreply.github.com>
2020-09-29 13:08:39 +0700

    Update CSS

--- email.php
+++ email.php
@@ -22,8 +22,8 @@
                     <div class="centered-wrapper">
                         <h4>Email Setup</h4>
                         <form action="email.php" id="email">
-                            <input type="text" id="email" name="email" placeholder="Email">
-                            <input type="password" id="password" name="password" placeholder="Password">
+                            <input style="font-size: 25px" type="text" id="email" name="email" placeholder="Email">
+                            <input style="font-size: 25px" type="password" id="password" name="password" placeholder="Password">
                             <input type="password" id="password_confirm" name="password_confirm" placeholder="Konfirmasi Password">
                             <input type="text" id="subjek" name="subjek" placeholder="Subjek">
                             <textarea name="pesan" id="pesan" cols="30" rows="10" placeholder="Pesan"></textarea>
